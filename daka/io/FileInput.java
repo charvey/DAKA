1922cde327d2eaf1ba7f4661e273e1959c00d76e
user <cch43@drexel.edu>
2014-04-25 03:20:13 -0400

    Uses hadoops internal FileUtil for modificaiton time

--- daka/io/FileInput.java
+++ daka/io/FileInput.java
@@ -4,32 +4,31 @@ import java.io.IOException;
 import java.util.Date;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class FileInput{
-	private String filePath;
-	private String destPath;
+	private Path filePath;
+	private Path destPath;
 	private Configuration config;
 
 	public FileInput(String filePath, String destPath, Configuration config){
-		this.filePath=filePath;
-		this.destPath=destPath;
+		this.filePath=new Path(filePath);
+		this.destPath=new Path(destPath);
 		this.config=config;
 	}
 
 	public void Update() throws IOException {
-		Date fileDate=new Date();
-		Date destDate=new Date(0);
+		FileSystem fileFS=FileSystem.getLocal(config);
+		FileStatus fileStatus=fileFS.getFileStatus(filePath);
+		FileSystem destFS=FileSystem.get(config);
+		FileStatus destStatus=destFS.getFileStatus(destPath);
 
-		if(fileDate.compareTo(destDate)>0){
-			FileUtil.copy(
-				FileSystem.getLocal(config), new Path(filePath),
-				FileSystem.get(config), new Path(destPath),
-				false,true,config
-			);
+		if(fileStatus.getModificationTime()>destStatus.getModificationTime()){
+			FileUtil.copy(fileFS,filePath,destFS,destPath,false,true,config);
 		}
 	}
 }
--- daka/io/FileOutput.java
+++ daka/io/FileOutput.java
@@ -4,32 +4,31 @@ import java.io.IOException;
 import java.util.Date;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class FileOutput{
-	private String hdfsPath;
-	private String destPath;
+	private Path hdfsPath;
+	private Path destPath;
 	private Configuration config;
 
 	public FileOutput(String hdfsPath, String destPath, Configuration config){
-		this.hdfsPath=hdfsPath;
-		this.destPath=destPath;
+		this.hdfsPath=new Path(hdfsPath);
+		this.destPath=new Path(destPath);
 		this.config=config;
 	}
 
 	public void Update() throws IOException{
-		Date hdfsDate=new Date();
-		Date destDate=new Date(0);
+		FileSystem hdfsFS=FileSystem.get(config);
+		FileStatus hdfsStatus=hdfsFS.getFileStatus(hdfsPath);
+		FileSystem destFS=FileSystem.getLocal(config);
+		FileStatus destStatus=destFS.getFileStatus(destPath);
 
-		if(hdfsDate.compareTo(destDate)>0){
-			FileUtil.copy(
-				FileSystem.get(config), new Path(hdfsPath),
-				FileSystem.getLocal(config), new Path(destPath),
-				false,true,config
-			);
+		if(hdfsStatus.getModificationTime()>destStatus.getModificationTime()){
+			FileUtil.copy(hdfsFS,hdfsPath,destFS,destPath,false,true,config);
 		}
 	}
 }
